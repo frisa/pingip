108c35cbc3379d1d409420a056b235081bbbf0e5
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-10-12 21:35:15 +0200

    ping from stack overflow running on macOS

--- pingip.cpp
+++ pingip.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <netinet/ip_icmp.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 
-#define PING_PKT_S 64
-#define RECV_TIMEOUT 1
-
-using namespace std;
+#define PACKETSIZE 64
+#define SOL_IP 0
 
 struct icmphdr
 {
@@ -29,47 +29,21 @@ struct icmphdr
         u_int32_t gateway; /* gateway address */
         struct
         {
-            u_int16_t neco;
+            u_int16_t unused;
             u_int16_t mtu;
         } frag; /* path mtu discovery */
     } un;
 };
-struct iphdr
+
+struct packet
 {
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    unsigned int ihl : 4;
-    unsigned int version : 4;
-#elif __BYTE_ORDER == __BIG_ENDIAN
-    unsigned int version : 4;
-    unsigned int ihl : 4;
-#else
-#error "Please fix <bits/endian.h>"
-#endif
-    u_int8_t tos;
-    u_int16_t tot_len;
-    u_int16_t id;
-    u_int16_t frag_off;
-    u_int8_t ttl;
-    u_int8_t protocol;
-    u_int16_t check;
-    u_int32_t saddr;
-    u_int32_t daddr;
-    /*The options start here. */
+    struct icmphdr hdr;
+    char msg[PACKETSIZE - sizeof(struct icmphdr)];
 };
 
-struct iphdr *ip;
-struct icmphdr *icmp;
-
-void setupPacket(int seq_no, char *packet)
-{
-    icmp = (struct icmphdr *)(packet + sizeof(struct iphdr));
-    icmp->type = ICMP_ECHO;
-    icmp->code = 0;
-    icmp->un.echo.sequence = seq_no;
-    icmp->un.echo.id = getpid();
-    icmp->checksum = 0;
-    icmp->checksum = 0;
-}
+int pid = -1;
+struct protoent *proto = NULL;
+int cnt = 1;
 
 unsigned short checksum(void *b, int len)
 {
@@ -87,78 +61,75 @@ unsigned short checksum(void *b, int len)
     return result;
 }
 
-int main(int argc, char *argv[])
+int ping(char *adress)
 {
-    const char* target_ip;
-    if (argc != 2)
-    {
-        cout << "Usage: " << argv[0] << " <destination_IP>" << endl;
-        std::string default_host = "www.google.com";
-        cout << "default host: " << default_host;
-        target_ip = default_host.c_str();
-    }
-    else
-    {
-        target_ip = argv[1];
-    }
+    const int val = 255;
+    int i, sd;
+    struct packet pckt;
+    struct sockaddr_in r_addr;
+    int loop;
+    struct hostent *hname;
+    struct sockaddr_in addr_ping, *addr;
 
-    int raw_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (raw_sock < 0)
+    pid = getpid();
+    proto = getprotobyname("ICMP");
+    hname = gethostbyname(adress);
+    bzero(&addr_ping, sizeof(addr_ping));
+    addr_ping.sin_family = hname->h_addrtype;
+    addr_ping.sin_port = 0;
+    addr_ping.sin_addr.s_addr = *(long *)hname->h_addr;
+
+    addr = &addr_ping;
+
+    sd = socket(PF_INET, SOCK_RAW, proto->p_proto);
+    if (sd < 0)
     {
-        std::cout << "error: " << errno;
-        perror("Socket Error");
+        perror("socket");
+        return 1;
+    }
+    if (setsockopt(sd, SOL_IP, IP_TTL, &val, sizeof(val)) != 0)
+    {
+        perror("Set TTL option");
+        return 1;
+    }
+    if (fcntl(sd, F_SETFL, O_NONBLOCK) != 0)
+    {
+        perror("Request nonblocking I/O");
         return 1;
     }
 
-    char packet[PING_PKT_S];
-    setupPacket(0, packet);
-
-    struct sockaddr_in dest;
-    bzero(&dest, sizeof(dest));
-    dest.sin_family = AF_INET;
-    dest.sin_port = 0;
-    dest.sin_addr.s_addr = inet_addr(target_ip);
-
-    cout << "PING " << target_ip << " (" << target_ip << "): " << PING_PKT_S << " data bytes" << endl;
-
-    int seq_no = 0;
-    while (true)
+    for (loop = 0; loop < 10; loop++)
     {
-        icmp->un.echo.sequence = seq_no++;
-        icmp->checksum = 0;
-        icmp->checksum = checksum((void *)icmp, PING_PKT_S);
-        int send_result = sendto(raw_sock, packet, PING_PKT_S, 0, (struct sockaddr *)&dest, sizeof(dest));
-        if (send_result == -1)
+
+        socklen_t len = sizeof(r_addr);
+
+        if (recvfrom(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *)&r_addr, &len) > 0)
         {
-            perror("Packet Send Error");
+            return 0;
         }
 
-        char recv_buffer[PING_PKT_S];
-        struct sockaddr_in recv_addr;
-        socklen_t addr_len = sizeof(recv_addr);
-        struct timeval timeout;
-        timeout.tv_sec = RECV_TIMEOUT;
-        timeout.tv_usec = 0;
+        bzero(&pckt, sizeof(pckt));
+        pckt.hdr.type = ICMP_ECHO;
+        pckt.hdr.un.echo.id = pid;
+        for (i = 0; i < sizeof(pckt.msg) - 1; i++)
+            pckt.msg[i] = i + '0';
+        pckt.msg[i] = 0;
+        pckt.hdr.un.echo.sequence = cnt++;
+        pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
+        if (sendto(sd, &pckt, sizeof(pckt), 0, (struct sockaddr *)addr, sizeof(*addr)) <= 0)
+            perror("sendto");
 
-        if (setsockopt(raw_sock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&timeout, sizeof(timeout)) < 0)
-        {
-            perror("Set Socket Timeout Error");
-            return 1;
-        }
-
-        ssize_t recv_result = recvfrom(raw_sock, recv_buffer, PING_PKT_S, 0, (struct sockaddr *)&recv_addr, &addr_len);
-        if (recv_result < 0)
-        {
-            cout << "Request timed out." << endl;
-        }
-        else
-        {
-            cout << PING_PKT_S << " bytes from " << target_ip << ": icmp_seq=" << seq_no << " ttl=64 time=" << recv_result << " ms" << endl;
-        }
-
-        sleep(1); // Wait for 1 second before sending the next packet.
+        usleep(300000);
     }
 
-    close(raw_sock);
+    return 1;
+}
+
+int main()
+{
+    if (ping("www.google.com"))
+        printf("Ping is not OK. \n");
+    else
+        printf("Ping is OK. \n");
     return 0;
 }
